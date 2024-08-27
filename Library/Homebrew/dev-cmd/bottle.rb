bdb954056237233474a964be203d8f564edc9c53
Michael Cho <michael@michaelcho.dev>
2024-08-27 09:27:47 -0400

    dev-cmd/bottle: use iso8601 for compliance with spec

--- Library/Homebrew/dev-cmd/bottle.rb
+++ Library/Homebrew/dev-cmd/bottle.rb
@@ -685,7 +685,8 @@ module Homebrew
               "root_url" => bottle.root_url,
               "cellar"   => bottle_cellar.to_s,
               "rebuild"  => bottle.rebuild,
-              "date"     => Pathname(filename.to_s).mtime.strftime("%F"),
+              # date is used for org.opencontainers.image.created which is an RFC 3339 date-time
+              "date"     => Pathname(filename.to_s).mtime.utc.iso8601,
               "tags"     => {
                 bottle_tag.to_s => {
                   "filename"        => filename.url_encode,
